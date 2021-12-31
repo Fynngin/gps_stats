d6d0a785b473a0aecb2a59f4c2cd9653d0a6e631
Fynn Knoop <fknoop@techfak.uni-bielefeld.de>
2021-12-31 09:41:04 +0100

    Init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
+import { ImmutableAssetStatusCodec, ImmutableMethodResults, ImmutableXClient } from '@imtbl/imx-sdk';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const tokenAddress = '0x6e8ba426586a96935c6b91fd12206557df6e3ec1';
+    const [imxClient, setImxClient] = useState<ImmutableXClient>();
+    useEffect(() => {
+        const init = async () => {
+            const client = await ImmutableXClient.build({ publicApiUrl: 'https://api.x.immutable.com/v1' })
+            setImxClient(client);
+        }
+        init();
+    }, [])
+
+    useEffect(() => {
+        getOwners();
+    }, [imxClient])
+
+    async function getOwners() {
+        if (!imxClient)
+            return;
+        let req_cursor = '';
+        let assets: any = [];
+        while (true) {
+            const res: ImmutableMethodResults.ImmutableGetAssetsResult = await imxClient.getAssets({
+                collection: tokenAddress,
+                page_size: 200,
+                cursor: req_cursor
+            })
+
+            if (!res.remaining)
+                break;
+            
+            req_cursor = res.cursor;
+            assets = assets.concat(res.result);
+        }
+        console.log(assets.length);
+    }
+
+    return (
+        <div className="App">
+        </div>
+    );
 }
 
 export default App;
