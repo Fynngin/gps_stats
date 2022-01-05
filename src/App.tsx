427322cad7710f4724457793c70910d879a39e29
Fynn Knoop <fynn.knoop@gmail.com>
2022-01-05 18:32:06 +0100

    Finished getting circulation numbers

--- src/App.tsx
+++ src/App.tsx
@@ -27,23 +27,60 @@ interface ItemInfo {
     owners: Set<string>
 }
 
+interface GearRarity {
+    type: 'Prime' | 'Lume' | 'Irid' | 'Alloy' | 'Core'
+    totalOwners: Set<string>
+    lockerOwners: Set<string>
+    lockerCirculation: number
+    lockerInventory: number
+    gearOwners: Set<string>
+    gearCirculation: number
+    gearInventory: number
+    floor?: number
+}
+
 function App() {
     const tokenAddress = '0x6e8ba426586a96935c6b91fd12206557df6e3ec1';
     const burnAddress = '0x12e923ef5d56f83d874d8d29dc98a7e83a616b19';
     const [loadingAssets, setLoadingAssets] = useState<Boolean>(false);
     const [allItems, setAllItems] = useState<{[key: number]: ItemInfo}>({});
+    const [totalOwners, setTotalOwners] = useState<Set<string>>(new Set());
+    const [coreInfo, setCoreInfo] = useState<GearRarity>(getInitObject('Core'));
+    const [alloyInfo, setAlloyInfo] = useState<GearRarity>(getInitObject('Alloy'));
+    const [iridInfo, setIridInfo] = useState<GearRarity>(getInitObject('Irid'));
+    const [lumeInfo, setLumeInfo] = useState<GearRarity>(getInitObject('Lume'));
+    const [primeInfo, setPrimeInfo] = useState<GearRarity>(getInitObject('Prime'));
 
-    useEffect(() => {
-        const init = async () => {
-            const client = await ImmutableXClient.build({ publicApiUrl: 'https://api.x.immutable.com/v1' })
-            getOwners(client);
-        }
-        init();
-    }, [])
+    function getInitObject(_type: 'Prime' | 'Lume' | 'Irid' | 'Alloy' | 'Core'): GearRarity {
+        return {
+            type: _type, 
+            totalOwners: new Set(), 
+            lockerOwners: new Set(), 
+            lockerCirculation: 0, 
+            lockerInventory: 0, 
+            gearOwners: new Set(), 
+            gearCirculation: 0, 
+            gearInventory: 0
+        };
+    }
+
+    async function handleInitButtonClick() {
+        const client = await ImmutableXClient.build({ publicApiUrl: 'https://api.x.immutable.com/v1' })
+        getOwners(client);
+    }
+
+    function resetVariables() {
+        setCoreInfo(getInitObject('Core'));
+        setAlloyInfo(getInitObject('Alloy'));
+        setIridInfo(getInitObject('Irid'));
+        setLumeInfo(getInitObject('Lume'));
+        setPrimeInfo(getInitObject('Prime'));
+    }
 
     async function getOwners(imxClient: ImmutableXClient) {
         let req_cursor = '';
         setLoadingAssets(true);
+        resetVariables();
         let tempItems: {[key: number]: ItemInfo} = allItems;
         while (true) {
             const res: ImmutableMethodResults.ImmutableGetAssetsResult = await imxClient.getAssets({
@@ -61,6 +98,46 @@ function App() {
                 const metadata = asset.metadata as Metadata;
                 const templateId = metadata.cardTemplateId ? metadata.cardTemplateId : metadata.packTemplateId;
 
+                setTotalOwners(t => t.add(asset.user as string));
+
+                if (metadata.name.match('Locker')) {
+                    switch (metadata.name.split(' ')[0]) {
+                        case 'Core':
+                            setCoreInfo(i => ({...i, lockerOwners: i.lockerOwners.add(asset.user as string), lockerCirculation: (i.lockerCirculation + 1), totalOwners: i.totalOwners.add(asset.user as string)}));
+                            break;
+                        case 'Alloy':
+                            setAlloyInfo(i => ({...i, lockerOwners: i.lockerOwners.add(asset.user as string), lockerCirculation: (i.lockerCirculation + 1), totalOwners: i.totalOwners.add(asset.user as string)}));
+                            break;
+                        case 'Irid':
+                            setIridInfo(i => ({...i, lockerOwners: i.lockerOwners.add(asset.user as string), lockerCirculation: (i.lockerCirculation + 1), totalOwners: i.totalOwners.add(asset.user as string)}));
+                            break;
+                        case 'Lume':
+                            setLumeInfo(i => ({...i, lockerOwners: i.lockerOwners.add(asset.user as string), lockerCirculation: (i.lockerCirculation + 1), totalOwners: i.totalOwners.add(asset.user as string)}));
+                            break;
+                        case 'Prime':
+                            setPrimeInfo(i => ({...i, lockerOwners: i.lockerOwners.add(asset.user as string), lockerCirculation: (i.lockerCirculation + 1), totalOwners: i.totalOwners.add(asset.user as string)}));
+                            break;
+                    }
+                } else {
+                    switch (metadata.Type) {
+                        case 'Core':
+                            setCoreInfo(i => ({...i, gearOwners: i.gearOwners.add(asset.user as string), gearCirculation: (i.gearCirculation + 1), totalOwners: i.totalOwners.add(asset.user as string)}));
+                            break;
+                        case 'Alloy':
+                            setAlloyInfo(i => ({...i, gearOwners: i.gearOwners.add(asset.user as string), gearCirculation: (i.gearCirculation + 1), totalOwners: i.totalOwners.add(asset.user as string)}));
+                            break;
+                        case 'Irid':
+                            setIridInfo(i => ({...i, gearOwners: i.gearOwners.add(asset.user as string), gearCirculation: (i.gearCirculation + 1), totalOwners: i.totalOwners.add(asset.user as string)}));
+                            break;
+                        case 'Lume':
+                            setLumeInfo(i => ({...i, gearOwners: i.gearOwners.add(asset.user as string), gearCirculation: (i.gearCirculation + 1), totalOwners: i.totalOwners.add(asset.user as string)}));
+                            break;
+                        case 'Prime':
+                            setPrimeInfo(i => ({...i, gearOwners: i.gearOwners.add(asset.user as string), gearCirculation: (i.gearCirculation + 1), totalOwners: i.totalOwners.add(asset.user as string)}));
+                            break;
+                    }
+                }
+
                 if (!templateId) continue;
 
                 let _old = tempItems[templateId];
@@ -71,7 +148,6 @@ function App() {
                         owners: new Set([asset.user])
                     } as ItemInfo;
                 } else {
-                    // tempItems[templateId].assets.push(asset);
                     tempItems[templateId].circulation++;
                     tempItems[templateId].owners.add(asset.user); 
                 }
@@ -89,26 +165,62 @@ function App() {
     return (
         <div className="App">
             {loadingAssets ? <h2>LOADING ASSETS...</h2> : null}
-            {Object.values(test).length > 0 ?
-                <table>
-                    <thead>
-                        <tr>
-                            <th></th>
-                            <th>Total Supply</th>
-                            <th># Unique Holders</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {Object.values(test).map((val: ItemInfo, idx: number) => (
-                            <tr key={idx}>
-                                <td>{val.assets[0].name}</td>
-                                <td>{val.circulation}</td>
-                                <td>{val.owners.size}</td>
-                            </tr>
-                        ))}
-                    </tbody>
-                </table>
-            : null}
+            <button onClick={handleInitButtonClick}>Reload Items</button>
+            <h3>Total Owners: {totalOwners.size}</h3>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Type</th>
+                        <th>Total Holders</th>
+                        <th>Gear Holders</th>
+                        <th>Gear Circulation</th>
+                        <th>Locker Holders</th>
+                        <th>Locker Circulation</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr style={{background: '#fd6d30'}}>
+                        <td>Prime</td>
+                        <td>{primeInfo.totalOwners.size}</td>
+                        <td>{primeInfo.gearOwners.size}</td>
+                        <td>{primeInfo.gearCirculation}</td>
+                        <td>{primeInfo.lockerOwners.size}</td>
+                        <td>{primeInfo.lockerCirculation}</td>
+                    </tr>
+                    <tr style={{background: '#e34ef0'}}>
+                        <td>Lume</td>
+                        <td>{lumeInfo.totalOwners.size}</td>
+                        <td>{lumeInfo.gearOwners.size}</td>
+                        <td>{lumeInfo.gearCirculation}</td>
+                        <td>{lumeInfo.lockerOwners.size}</td>
+                        <td>{lumeInfo.lockerCirculation}</td>
+                    </tr>
+                    <tr style={{background: '#4b88fd'}}>
+                        <td>Irid</td>
+                        <td>{iridInfo.totalOwners.size}</td>
+                        <td>{iridInfo.gearOwners.size}</td>
+                        <td>{iridInfo.gearCirculation}</td>
+                        <td>{iridInfo.lockerOwners.size}</td>
+                        <td>{iridInfo.lockerCirculation}</td>
+                    </tr>
+                    <tr style={{background: '#00e5ad'}}>
+                        <td>Alloy</td>
+                        <td>{alloyInfo.totalOwners.size}</td>
+                        <td>{alloyInfo.gearOwners.size}</td>
+                        <td>{alloyInfo.gearCirculation}</td>
+                        <td>{alloyInfo.lockerOwners.size}</td>
+                        <td>{alloyInfo.lockerCirculation}</td>
+                    </tr>
+                    <tr style={{background: ''}}>
+                        <td>Core</td>
+                        <td>{coreInfo.totalOwners.size}</td>
+                        <td>{coreInfo.gearOwners.size}</td>
+                        <td>{coreInfo.gearCirculation}</td>
+                        <td>{coreInfo.lockerOwners.size}</td>
+                        <td>{coreInfo.lockerCirculation}</td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
     );
 }
